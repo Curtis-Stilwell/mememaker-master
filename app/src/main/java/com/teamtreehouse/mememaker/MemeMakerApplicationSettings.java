6d260c3d10ff2c3a36ccef98d4ac1fc09e70dac2
Curtis-Stilwell <curtisstilwell1@Curtiss-MacBook-Pro.local>
2018-03-04 11:07:03 -0600

    initial commit

---
+++ app/src/main/java/com/teamtreehouse/mememaker/MemeMakerApplicationSettings.java
@@ -0,0 +1,27 @@
+package com.teamtreehouse.mememaker;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.teamtreehouse.mememaker.utils.StorageType;
+
+public class MemeMakerApplicationSettings {
+    SharedPreferences mSharedPreferences;
+
+    public MemeMakerApplicationSettings(Context context) {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public String getStoragePreference() {
+        return mSharedPreferences.getString("Storage", StorageType.INTERNAL);
+    }
+
+
+    public void setSharedPreferences(String storageType) {
+        mSharedPreferences
+                .edit()
+                .putString("Storage",storageType)
+                .apply();
+    }
+}
---
+++ app/src/main/java/com/teamtreehouse/mememaker/ui/fragments/MemeSettingsFragment.java
@@ -0,0 +1,14 @@
+package com.teamtreehouse.mememaker.ui.fragments;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.teamtreehouse.mememaker.R;
+
+public class MemeSettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
